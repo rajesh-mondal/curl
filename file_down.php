b546021dc74f1f23f7ad5ca85f6e4e2eec99f5a9
Rajesh Mondal <30333445+rajesh-mondal@users.noreply.github.com>
2020-07-16 13:43:50 +0600

    Add files via upload

---
+++ file_down.php
@@ -0,0 +1,7 @@
+<?php
+//Remote file download
+set_time_limit(0);
+$ch = curl_init("https://images.unsplash.com/photo-1594389509808-5776006fd704");
+$filename = fopen("image2.jpg", "w");
+curl_setopt($ch, CURLOPT_FILE, $filename);
+curl_exec($ch);
